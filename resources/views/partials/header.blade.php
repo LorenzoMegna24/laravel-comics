1017944c25d7cb5d52e2fb58ae6a4600f70015a0
LorenzoMegna24 <megnalorenzo0@gmial.com>
2023-06-08 16:34:33 +0200

    Creazione partials

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,19 @@
+<nav>
+      <div class="logo">
+        <a href="/"><img src="../../public/img/dc-logo.png" alt=""></a>
+      </div>
+      <div>
+        <ul>
+          <li><a href="/">characters</a></li>
+          <li class="active"><a href="/">comics</a></li>
+          <li><a href="/">movies</a></li>
+          <li><a href="/">tv</a></li>
+          <li><a href="/">games</a></li>
+          <li><a href="/">collectibles</a></li>
+          <li><a href="/">videos</a></li>
+          <li><a href="/">fans</a></li>
+          <li><a href="/">news</a></li>
+          <li><a href="/">shop</a></li>
+        </ul>
+      </div>
+    </nav>
---
+++ resources/views/partials/main.blade.php
@@ -0,0 +1,30 @@
+<div class="jumbo">
+      <span>CURRENT SERIES</span>
+    </div>
+    <div class="content">
+      <!-- foreach -->
+    </div>
+    <div class="links">
+      <ul>
+        <li>
+          <a href="/"><img src="../../public/img/buy-comics-digital-comics.png" alt="">
+          <span>digital comics</span></a>
+        </li>
+        <li>
+          <a href="/"><img src="../../public/img/buy-comics-merchandise.png" alt="">
+          <span>dc merchandise</span></a>
+        </li>
+        <li>
+          <a href="/"><img src="../../public/img/buy-comics-subscriptions.png" alt="">
+          <span>subscription</span></a>
+        </li>
+        <li>
+          <a href="/"><img src="../../public/img/buy-comics-shop-locator.png" alt="">
+          <span>comics shop locator</span></a>
+        </li>
+        <li>
+          <a href="/"><img src="../../public/img/buy-dc-power-visa.svg" alt="">
+          <span>dc power visa</span></a>
+        </li>
+      </ul>
+    </div>
